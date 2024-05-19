562f17a2f538307546514ca1a82db84689549931
elMofaker <155926314+elMofaker@users.noreply.github.com>
2024-05-20 02:59:47 +0300

    add_sec_home

--- style/js/home.js
+++ style/js/home.js
@@ -16,3 +16,13 @@ ScrollReveal({
 ScrollReveal().reveal('.name', { delay: 250, origin: 'left', interval: 200});
 ScrollReveal().reveal('.right', { delay: 250, origin: 'right', interval: 150});
 ScrollReveal().reveal('.top', { delay: 250, origin: 'top', interval: 150});
+
+
+
+
+
+const logo = document.querySelectorAll("#logo path");
+
+for (let i = 0; i < logo.length; i++) {
+    console.log(`Letter ${i} length is ${logo[i].getTotalLength()}`);
+}
