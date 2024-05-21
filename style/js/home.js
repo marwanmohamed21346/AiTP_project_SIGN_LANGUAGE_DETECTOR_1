50f1096c7af63c5d4f057eee2994b33d2952d4d6
elMofaker <155926314+elMofaker@users.noreply.github.com>
2024-05-21 06:57:34 +0300

    f

--- style/js/home.js
+++ style/js/home.js
@@ -4,43 +4,10 @@
 //     console.log(`Letter ${i} length is ${logo[i].getTotalLength()}`);
 // }
 
-
-$(window).on('load', function() {
-  const spinnerAnimationDuration = 500;
-
-  // الكلاسات التي تريد إزالتها وإعادة إضافتها
-  const classesToToggle = ['name', 'top'];
-
-  // حدد كل عناصر المحتوى
-  const contentElements = $(".content");
-
-  // إزالة الكلاسات المحددة أثناء مرحلة اللودينج
-  contentElements.each(function() {
-    const $this = $(this);
-    classesToToggle.forEach(function(cls) {
-      $this.removeClass(cls);
-    });
-  });
-
-  // Fade out the loader after spinnerAnimationDuration
-  setTimeout(function() {
-    $(".loader").fadeOut(1000, function() {
-      // بعد اختفاء اللودر، أعد إضافة الكلاسات التي أزلتها وقم بتشغيل الكلاسات الخاصة بالأنيميشن
-      contentElements.each(function() {
-        const $this = $(this);
-
-        // إعادة إضافة الكلاسات المحددة
-        classesToToggle.forEach(function(cls) {
-          $this.addClass(cls);
-        });
-
-        // إضافة الكلاس الخاص بالأنيميشن
-        $this.removeClass('hidden').addClass('fade-in');
-      });
-    });
-  }, spinnerAnimationDuration);
+$(window).on("load", function () {
+  $(".loader").fadeOut(1000),
+  $(".content").fadeIn(1000)
 });
-
 
 // scroll anima
 ScrollReveal({
@@ -50,25 +17,28 @@ ScrollReveal({
   delay: 400,
 });
 
-ScrollReveal().reveal(".name", { delay: 250, origin: "left", interval: 200 });
+ScrollReveal().reveal(".left", { delay: 250, origin: "left", interval: 200 });
 ScrollReveal().reveal(".right", { delay: 250, origin: "right", interval: 150 });
-ScrollReveal().reveal(".top", { delay: 250, origin: "top", interval: 150 });
+ScrollReveal().reveal(".top", { delay: 250, origin: "top", interval: 250 });
 
-// add class if scroll
-// $(window).on("load", function () {
-//   $(".loadding-page").delay(6000).fadeOut(200);
-//   $(".cssload-box-loading").on("click", function () {
-//     $(".cssload-box-loading").fadeOut(200);
-//   });
-// });
+// slid show
 
-// // script.js
-
-// $(window).on('load', function() {
-//   const spinnerAnimationDuration = 2000;
-//   setTimeout(function() {
-//     $(".loader").fadeOut(1000);
-//     $(".content").removeClass('hidden').addClass('fade-in');
-//   }, spinnerAnimationDuration);
-// });
-
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    310: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
