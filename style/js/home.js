ee1a7b7777ad2cca2975c9d229e87a8adbdfef98
elMofaker <155926314+elMofaker@users.noreply.github.com>
2024-05-21 07:38:08 +0300

    س

--- style/js/home.js
+++ style/js/home.js
@@ -5,21 +5,23 @@
 // }
 
 $(window).on("load", function () {
-  $(".loader").fadeOut(1000),
-  $(".content").fadeIn(1000)
+  $(".content").removeClass('hidden').addClass('fade-in').fadeIn(1000);
+  $(".loader").fadeOut(3000, function () { 
+  });
 });
+
 
 // scroll anima
 ScrollReveal({
   // reset: true,
   distance: "80px",
   duration: 2000,
-  delay: 400,
+  // delay: 400,
 });
 
 ScrollReveal().reveal(".left", { delay: 250, origin: "left", interval: 200 });
 ScrollReveal().reveal(".right", { delay: 250, origin: "right", interval: 150 });
-ScrollReveal().reveal(".top", { delay: 250, origin: "top", interval: 250 });
+ScrollReveal().reveal(".top", { delay: 250, origin: "top", interval: 200 });
 
 // slid show
 
