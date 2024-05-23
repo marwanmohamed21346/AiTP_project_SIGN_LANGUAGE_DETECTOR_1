34fb12a0a5ea883b1bb6eac618a1b22050b60bfe
marwanmohamed <marwanmohamed2238@gmail.com>
2024-05-23 09:25:29 +0300

    all way lead to roma

--- main.py
+++ main.py
@@ -10,40 +10,56 @@ try:
 except Exception as e:
     print("error loading packages:",e)
 
-
 try:
-    model_path = "AITP_Training_code.pt"
-    model = load_model(model_path)
-    print(model.eval())
-
+    model_path = "Jo_elMofaker.pt"
+    model = Network()
+    print(model)
+    model.eval()
     print("Model loaded successfully")
 except Exception as e:
     print("Error loading model:", e)
-
 
 cam = cv2.VideoCapture(0)
 cam.set(3, 1280)  # Set width
 cam.set(4, 720)  # Set height
 
-
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpdraw = mp.solutions.drawing_utils
 
-
 signs = {
-    '0': 'A', '1': 'B', '2': 'C', '3': 'D', '4': 'E', '5': 'F', '6': 'G', '7': 'H', '8': 'I',
-    '10': 'K', '11': 'L', '12': 'M', '13': 'N', '14': 'O', '15': 'P', '16': 'Q', '17': 'R',
-    '18': 'S', '19': 'T', '20': 'U', '21': 'V', '22': 'W', '23': 'X', '24': 'Y'
+    '0': 'A',
+    '1': 'B', 
+    '2': 'C', 
+    '3': 'D', 
+    '4': 'E', 
+    '5': 'F', 
+    '6': 'G', 
+    '7': 'H', 
+    '8': 'I',
+    '10': 'K', 
+    '11': 'L', 
+    '12': 'M', 
+    '13': 'N', 
+    '14': 'O', 
+    '15': 'P', 
+    '16': 'Q',
+    '17': 'R',
+    '18': 'S',
+    '19': 'T',
+    '20': 'U',
+    '21': 'V',
+    '22': 'W',
+    '23': 'X',
+    '24': 'Y',
+    ' ' : 'NO sign'
     }
-
 
 def Transform(image):
     image = image / 255.0
     image = np.expand_dims(image, axis=0)
     image = np.expand_dims(image, axis=0)
     return torch.from_numpy(image).float()
-
 
 while True:
     ret, frame = cam.read()
@@ -54,15 +70,15 @@ while True:
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = hands.process(framergb)
     hand_landmarks = result.multi_hand_landmarks
-
+    
     if hand_landmarks:
         for handLMs in hand_landmarks:
             mpdraw.draw_landmarks(frame, handLMs, mpHands.HAND_CONNECTIONS)
-
-        if len(hand_landmarks) == 2:
-            x_max, y_max = 0, 0
-            x_min, y_min = w, h
-            combined = [lm for handLMs in hand_landmarks for lm in handLMs.landmark]
+            
+            if len(hand_landmarks) == 1:
+                x_max, y_max = 0, 0
+                x_min, y_min = w, h
+                combined = [lm for handLMs in hand_landmarks for lm in handLMs.landmark]
 
             for lm in combined:
                 x, y = int(lm.x * w), int(lm.y * h)
@@ -80,7 +96,7 @@ while True:
         x_min, x_max = max(x_min - 10, 0), min(x_max + 10, w)
         y_min, y_max = max(y_min - 10, 0), min(y_max + 10, h)
         cropped_image = frame[y_min:y_max, x_min:x_max]
-
+        
         try:
             image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
             image = np.array(cv2.resize(image, (28, 28)))
@@ -89,7 +105,7 @@ while True:
             with torch.no_grad():
                 prediction = model(image)
             prediction = torch.nn.functional.softmax(prediction, dim=1)
-            i = prediction.argmax(dim=-1).cpu()
+            i = prediction.argmax(dim=-1).cuda()
             print(i)
             label = signs[str(i.item())]
         except Exception as e:
